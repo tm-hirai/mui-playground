989df5e30f7469e647c961932999aeb2cf35e520
tm-hirai <tm-hirai@kddi.com>
2025-09-19 15:00:17 +0900

    feat: muiのTimeClockをお試し

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Typography } from "@mui/material";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimeClock } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+import { useState } from "react";
+// import dayjs from "dayjs";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [min, setMin] = useState<dayjs.Dayjs | null>(null);
+  const [max, setMax] = useState<dayjs.Dayjs | null>(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box>
+      <DemoContainer components={["TimeClock", "TimeClock"]}>
+        <Typography variant="h6" gutterBottom>
+          TimeClock
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          選択中の時間 {min?.format("HH:mm") ?? "--:--"} -{" "}
+          {max?.format("HH:mm") ?? "--:--"}
+        </Typography>
+      </DemoContainer>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ja">
+        <DemoContainer components={["TimeClock", "TimeClock"]}>
+          <DemoItem label="Mobile variant">
+            <TimeClock
+              ampm={false}
+              view="hours"
+              value={min}
+              maxTime={max ?? undefined}
+              onChange={(newValue) => setMin(newValue)}
+            />
+          </DemoItem>
+          <DemoItem label="Mobile variant">
+            <TimeClock
+              ampm={false}
+              view="hours"
+              value={max}
+              minTime={min ?? undefined}
+              onChange={(newValue) => setMax(newValue)}
+            />
+          </DemoItem>
+        </DemoContainer>
+      </LocalizationProvider>
+    </Box>
+  );
 }
 
-export default App
+export default App;
